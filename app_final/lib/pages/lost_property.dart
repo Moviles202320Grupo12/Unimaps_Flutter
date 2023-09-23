969f803a1b9fbfc85238278ee0615c8468c74b31
Mateo Gutierrez <mgutierrezca@unal.edu.co>
2023-09-22 22:59:22 -0500

    Deslizar objetos perdidos

--- app_final/lib/pages/lost_property.dart
+++ app_final/lib/pages/lost_property.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 
 class LostProperty extends StatelessWidget {
-  @override // Just for know we are overriding
-
-  // Widget creation
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(home: LostPropertyHome());
   }
 }
 
 class LostPropertyHome extends StatelessWidget {
-  // ignore: constant_identifier_names
   static const black_titles = Color(0x00090909);
 
   const LostPropertyHome({super.key});
@@ -20,11 +17,13 @@ class LostPropertyHome extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: (const Text('Objetos Perdidos',
-            style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'Urbanist',
-                fontWeight: FontWeight.bold))),
+        title: const Text(
+          'Objetos Perdidos',
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Urbanist',
+              fontWeight: FontWeight.bold),
+        ),
         backgroundColor: Colors.white,
         elevation: 0.0,
       ),
@@ -38,15 +37,14 @@ class LostPropertyHome extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 15.0),
             child: TextField(
               onChanged: (value) {
-                setState(() {
-                  var searchString = value.toLowerCase();
-                });
+                // Handle search logic here
               },
               decoration: InputDecoration(
                 labelText: '',
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0)),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
                 contentPadding:
                     EdgeInsets.symmetric(vertical: 3.0, horizontal: 2.0),
               ),
@@ -55,21 +53,21 @@ class LostPropertyHome extends StatelessWidget {
           const SizedBox(height: 10),
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Text("Categorias",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'Urbanist',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20)),
+            child: Text(
+              "Categorias",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Urbanist',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
           ),
           SizedBox(height: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                onPressed: () {
-                  // Acción del botón 1
-                },
+                onPressed: () {},
                 style: ElevatedButton.styleFrom(
                     primary: Colors.black, minimumSize: Size(40, 40)),
                 child: const Text("Celulares",
@@ -121,88 +119,29 @@ class LostPropertyHome extends StatelessWidget {
             ],
           ),
           Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Dos elementos por fila
-              ),
-              itemCount: 4, // Cuatro elementos en total
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset('lib/imgs/el_logo.png'),
-                      const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("Iphone 13",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18)),
-                                  Text(
-                                      "Encontrado:", // Nuevo texto debajo de "Iphone 13"
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          color: Color(0xBBBB87400))),
-                                  Text(
-                                      "Martes", // Nuevo texto debajo de "Iphone 13"
-                                      style: TextStyle(
-                                        color: Color(0xFFF6A700),
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                      )),
-                                  Text(" ", // Nuevo texto debajo de "Iphone 13"
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                      )),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Icon(Icons.location_on_outlined,
-                                      color: Color(
-                                          0xBBBB87400)), // Ícono al lado del texto
-                                  SizedBox(
-                                      width:
-                                          2), // Espacio entre el ícono y el texto adicional
-                                  Text("RGD",
-                                      style: TextStyle(
-                                          color: Color(0xBBBB87400),
-                                          fontSize:
-                                              18)), // Texto adicional al lado del ícono
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+            child: PageView(
+              children: [
+                buildGridView('Category 1'),
+                buildGridView('Category 2'),
+                buildGridView('Category 3'),
+                buildGridView('Category 4'),
+                // Add more categories as needed
+              ],
             ),
           ),
           Center(
             child: ElevatedButton(
-              onPressed: () {
-                // Acción del botón
-              },
+              onPressed: () {},
               style: ElevatedButton.styleFrom(
                   primary: Colors.black, minimumSize: Size(40, 40)),
-              child: Text("Reportar Objeto perdido",
-                  style: TextStyle(
-                      color: Color(0xFFF6A700),
-                      fontFamily: 'Urbanist',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20)),
+              child: const Text(
+                "Reportar Objeto perdido",
+                style: TextStyle(
+                    color: Color(0xFFF6A700),
+                    fontFamily: 'Urbanist',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
             ),
           ),
         ],
@@ -210,5 +149,63 @@ class LostPropertyHome extends StatelessWidget {
     );
   }
 
-  void setState(Null Function() param0) {}
+  Widget buildGridView(String category) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: 4,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset('lib/imgs/el_logo.png'),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Iphone 13",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text(
+                            "Encontrado:",
+                            style: TextStyle(
+                                fontSize: 15, color: Color(0xBBBB87400)),
+                          ),
+                          // ... (other texts)
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on_outlined,
+                            color: Color(0xBBBB87400),
+                          ),
+                          SizedBox(width: 2),
+                          Text(
+                            "RGD",
+                            style: TextStyle(
+                                color: Color(0xBBBB87400), fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
