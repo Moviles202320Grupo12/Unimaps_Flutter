1c0749a876498d8d5ff7ed3c1b3970e774b72bba
Mateo Gutierrez <mgutierrezca@unal.edu.co>
2023-10-25 10:27:10 -0500

    nuevo inicio

--- stuzonefinal/lib/src/constants/colors.dart
+++ stuzonefinal/lib/src/constants/colors.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 /* -- LIST OF ALL COLORS -- */
 
 const tPrimaryColor = Color(0xFFFFE400);
@@ -11,11 +10,7 @@ const tWhiteColor = Colors.white;
 const tDarkColor = Color(0xff000000);
 const tCardBgColor = Color(0xFFF7F5F1);
 
-
-
 // -- ON-BOARDING COLORS
 const tOnBoardingPage1Color = Colors.white;
 const tOnBoardingPage2Color = Color(0xfffddcdf);
 const tOnBoardingPage3Color = Color(0xffffdcbd);
-
-
